708e3eedae1421323aac27dc93e789550dabdc3b
Raven0 <azh.zhafir@gmail.com>
2021-03-09 18:40:48 +0700

    prime

--- src/com/binus/Main.java
+++ src/com/binus/Main.java
@@ -1,15 +1,39 @@
 package com.binus;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = "This is a sample sentence.";
-        String[] words = s.split("\\s+");
+        List<Integer> primes = new ArrayList<>();
+        int n = 1;
+        while (primes.size() != 5) {
+            if (isPrime(n)){
+                primes.add(n);
+            }
+            n++;
+        }
 
-        IntStream.range(0, words.length).forEach(i -> print(i + ": " + words[i]));
-        print("Total word: "+words.length);
+        print(primes.toString());
+    }
+
+    static boolean isPrime(int n)
+    {
+        boolean result = true;
+
+        // untuk validasi angka 1
+        if (n < 2) {
+            result = false;
+        }
+
+        for (int i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     private static void print(String args){
