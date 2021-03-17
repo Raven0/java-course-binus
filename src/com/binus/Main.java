d5577a34c6433343a14b6a67ef8900acae33b81c
Raven0 <azh.zhafir@gmail.com>
2021-03-17 14:32:43 +0700

    assignment

--- src/com/binus/Main.java
+++ src/com/binus/Main.java
@@ -1,91 +1,79 @@
 package com.binus;
 
-import com.binus.models.Pet;
-
-import java.util.List;
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
-    private static List<Pet> petList;
+    private static final DecimalFormat formatter = new DecimalFormat("###,###,###.00");
 
     public static void main(String[] args) {
-        int menu;
+        String vType, tIn, tOut;
+
+        //testing();
 
         do {
-            mainMenu();
-            menu = scanInt("Choice: ");
-            switch (menu) {
-                case 1:
-                    addPetMenu();
-                    break;
-                case 2:
-                    viewPetListMenu();
-                    break;
-                case 3:
-                    updatePetMenu();
-                    break;
-                case 4:
-                    treatPetMenu();
-                    break;
-            }
-        } while (menu != 5);
+            vType = scanString("Jenis Kendaraan [Motor/Mobil]:");
+        } while (!vType.equals("Motor") && !vType.equals("Mobil"));
+
+        do {
+            tIn = scanString("IN [hh.mm]:");
+        } while (isValidTime(tIn));
+
+        do {
+            tOut = scanString("OUT [hh.mm]:");
+        } while (isValidTime(tOut));
+
+        calculateParkFee(
+                vType,
+                countHours(tIn, tOut)
+        );
+
     }
 
-    private static void mainMenu() {
-        print("+======================+");
-        print("1. Add Sick Pet");
-        print("2. View Sick Pet List");
-        print("3. Update Sick Pet");
-        print("4. Treat Sick Pet");
-        print("5. Exit Program");
-        print("+======================+");
-    }
-
-    private static void addPetMenu() {
-        Pet newPet = new Pet();
-
-        do {
-            newPet.setHealth(scanInt("Input pet Health [1 - 90]"));
-        } while (newPet.getHealth() >= 1 && newPet.getHealth() <= 90);
-
-        print(newPet.getHealth().toString());
-
-        do {
-            newPet.setName(scanString("Input pet Name [5 - 15 characters]"));
-        } while (newPet.getName().length() >= 5 && newPet.getName().length() <= 15);
-
-        do {
-            newPet.setType(scanString("Input pet Type [kit | puppy | kitten]"));
-        } while (newPet.getType().equals("kit") || newPet.getType().equals("puppy") || newPet.getType().equals("kitten"));
-
-        newPet.setType(scanString("Input pet Type [kit | puppy | kitten]"));
-
-        petList.add(newPet);
-    }
-
-    private static void viewPetListMenu() {
-        if (petList.size() != 0) {
-            for (Pet pet : petList) {
-                print(pet.toString());
-            }
-        } else {
-            print("Empty List");
+    @SuppressWarnings("IntegerMultiplicationImplicitCastToLong")
+    private static void calculateParkFee(String type, int hours) {
+        switch (type) {
+            case "Motor":
+                if (hours >= 2) {
+                    print("Rp " + formatter.format((3000 + (4000 * (hours - 1) ))));
+                } else {
+                    print("Rp 3,000.00");
+                }
+                break;
+            case "Mobil":
+                if (hours >= 2) {
+                    print("Rp " + formatter.format((5000 + (6000 * (hours - 1) ))));
+                } else {
+                    print("Rp 5,000.00");
+                }
+                break;
         }
     }
 
-    private static void updatePetMenu() {
-
+    private static int countHours(String tStart, String tEnd) {
+        return Integer.parseInt(tEnd.substring(0, 2)) - Integer.parseInt(tStart.substring(0, 2));
     }
 
-    private static void treatPetMenu() {
+    private static boolean isValidTime(String time) {
+        String regex = "([01]?[0-9]|2[0-3]).[0-5][0-9]";
+        Pattern p = Pattern.compile(regex);
 
+        if (time == null) {
+            return true;
+        }
+
+        Matcher m = p.matcher(time);
+
+        return !m.matches();
     }
 
     private static void print(String args) { System.out.println(args); }
 
-    public static String scanString(String msg) {
+    private static String scanString(String msg) {
         String value;
 
         try {
@@ -99,19 +87,22 @@ public class Main {
         return value;
     }
 
-    @SuppressWarnings("SameParameterValue")
-    public static Integer scanInt(String msg) {
-        int value;
+    @SuppressWarnings("unused")
+    private static void testing() {
+        calculateParkFee(
+                "Mobil",
+                countHours("07:45", "09:01")
+        );
 
-        try {
-            print(msg);
-            value = scanner.nextInt();
-        } catch (Exception x) {
-            print(msg);
-            value = scanner.nextInt();
-        }
+        calculateParkFee(
+                "Motor",
+                countHours("07:45", "08:01")
+        );
 
-        return value;
+        calculateParkFee(
+                "Mobil",
+                countHours("07:45", "12:01")
+        );
     }
 
 }
